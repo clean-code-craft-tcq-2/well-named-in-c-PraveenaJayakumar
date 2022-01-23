8cbb0d9a2e9ac52c661f23899f8accc941853d9f
PraveenaJayakumar <90901057+PraveenaJayakumar@users.noreply.github.com>
2022-01-23 19:10:23 +0530

    Add files via upload

---
+++ Color_code_info_extract.h
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <assert.h>
+
+enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
+enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
+const int MAX_COLORPAIR_NAME_CHARS;
+
+typedef struct {
+    enum MajorColor majorColor;
+    enum MinorColor minorColor;
+} ColorPair;
+
+void testNumberToPair( int pairNumber, enum MajorColor expectedMajor, enum MinorColor expectedMinor);
+void testPairToNumber( enum MajorColor major, enum MinorColor minor, int expectedPairNumber);
+int GetPairNumberFromColor(const ColorPair* colorPair);
+ColorPair GetColorFromPairNumber(int pairNumber);
+void ColorPairToString(const ColorPair* colorPair, char* buffer);
