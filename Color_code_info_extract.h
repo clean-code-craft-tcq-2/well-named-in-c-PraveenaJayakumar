69d23de944cc929bc2eb634827857cd5508e5b46
PraveenaJayakumar <90901057+PraveenaJayakumar@users.noreply.github.com>
2022-01-23 20:24:45 +0530

    Update Color_code_info_extract.h

--- Color_code_info_extract.h
+++ Color_code_info_extract.h
@@ -4,6 +4,7 @@
 enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
 enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
 const int MAX_COLORPAIR_NAME_CHARS;
+#define MaxPossibleColorsInMajorOrMinor (5u)
 
 typedef struct {
     enum MajorColor majorColor;
