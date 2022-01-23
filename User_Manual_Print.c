deae1b1fdef6e1ed1e2037a7149045285f99f134
PraveenaJayakumar <90901057+PraveenaJayakumar@users.noreply.github.com>
2022-01-23 20:19:38 +0530

    Add files via upload

---
+++ User_Manual_Print.c
@@ -0,0 +1,34 @@
+#include "Color_code_info_extract.h"
+
+void To_print_WiringManualData(void){
+    unsigned int MajorColor = 0, MinorColor = 0;
+    unsigned int colorCode_order = 1;
+
+    
+    for(MajorColor = 0; MajorColor < MaxPossibleColorsInMajorOrMinor; MajorColor++)
+    {
+        for(MinorColor = 0; MinorColor < MaxPossibleColorsInMajorOrMinor; MinorColor++)
+        {
+             printf("\n*********************");
+             printf("\n Major Color : %s  ", MajorColorNames[MajorColor_index]);
+             printf("\n Minor Color : %s  ", MinorColorNames[MinorColor_index]);
+             printf("\n Color Code  : %d  ", colorCode);
+             printf("\n*********************");
+
+             print_data(colorCode_order,MajorColorNames[MajorColor],MinorColorNames[MinorColor_index]);
+             colorCode_order++;
+        }
+    }
+}
+
+void print_data(int codenum, char *Majorcolor , char *Minorcolor)
+{
+             printf("\n*********************");
+             printf("\n Major Color : %s  ", MajorColor);
+             printf("\n Minor Color : %s  ", MinorColor);
+             printf("\n Color Code  : %d  ", colornum);
+             printf("\n*********************");
+}
+
+
+
